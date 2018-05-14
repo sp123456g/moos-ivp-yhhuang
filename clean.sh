867cd08cee02c982bd9cfe28ce295a4084305ed4
sp123456g <36514538+sp123456g@users.noreply.github.com>
2018-05-13 21:05:14 -0400

    modify clean.sh to touch README file in lib and build

--- clean.sh
+++ clean.sh
@@ -9,5 +9,7 @@ find . -name '*~'  -print -exec rm -rfv {} \;
 find . -name '#*'  -print -exec rm -rfv {} \;
 find . -name '*.moos++'  -print -exec rm -rfv {} \;
 
+touch lib/README.txt
+touch build/README.txt
 #find . -name 'MOOSLog*'  -print -exec rm -rfv {} \;
 
