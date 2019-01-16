5286fdab9d78c2f8d89239be2fd5affea00a5e58
YEN-HSIANG HUANG <36514538+sp123456g@users.noreply.github.com>
2019-01-16 23:07:32 +0800

    add new field script and GUI toolbox

---
+++ missions/mission_Taiwan/pam/pam_keelung_river_with_tracking/shore_field_clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+DATE_TIME=$(date +%Y_%m%d_%H%M)
+NEW_FOLDER="Real_field_Data_Shore/$DATE_TIME"
+
+mv  LOG_* $NEW_FOLDER
+mv  Data_from_scp $NEW_FOLDER
+rm -f ./Spectrogram_data/final_P
+rm -f ./Spectrogram_data/original_P
+
+rm -f   *~
+rm -f   targ_*
+rm -f   .LastOpenedMOOSLogDirectory
+
